d53ff532b1cd5c9e144b58735fee5182f73f858a
HimeshLaddha <himeshladdha@gmail.com>
2025-06-23 20:01:01 +0530

    backend updated

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -1,52 +1,50 @@
 import User from "../models/User.js";
 import { Webhook } from "svix";
 
-const clerkWebhooks = async (req, res)=>{
+const clerkWebhooks = async ()=>{
     try {
-        // Creating svinx instance with clerk webhook secret.
-        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
-        // Getting headers
+        // Getting data from the webhook
         const headers = {
             "svix-id": req.headers["svix-id"],
             "svix-timestamp": req.headers["svix-timestamp"],
-            "svix-signature": req.headers["svix-signature"]
-        };
+            "svix-signature": req.headers["svix-signature"],
+        }
 
-        // Verifying headers
         await whook.verify(JSON.stringify(req.body), headers)
 
-        // getting data from request body
-        const {data, type} = req.body
+        // Getting data from body request
+        const {data, type} = req.body;
 
         const userData = {
             _id: data.id,
-            email: data.email_adresses[0].email_address,
+            email: data.email_addresses[0].email_address,
             username: data.first_name + " " + data.last_name,
-            image: data.image_url,
+            Image: data.image_url
         }
 
-        switch(type){
-            case "user.created": {
+        switch (type) {
+            case "user.created":{
                 await User.create(userData);
                 break;
             }
-            case "user.updated": {
-                await User.findByIdAndUpdate(data.id, userData);
+            case "user.updated":{
+                await User.findByIdAndUpdate(data._id, userData);
                 break;
             }
-            case "user.deleted": {
-                await User.findByIdAndDelete(data.id);
+            case "user.deleted":{
+                await User.findByIdAndDelete(data._id);
                 break;
             }
             default:
                 break;
         }
 
-        res.json({success: true, message: "Webhook Received"})
+        res.json({success: true, message: "Webhook eceived"})
     } catch (error) {
-        console.log(error.message);
         res.json({success: false, message: error.message});
+        console.log(error.message);
     }
 }
 
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import "dotenv/config";
+import "dotenv/config"
 import cors from "cors";
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from '@clerk/express'
@@ -14,13 +14,12 @@ app.use(cors())
 app.use(express.json())
 app.use(clerkMiddleware())
 
-// Api to listen clerk webhooks
 app.use("/api/clerk", clerkWebhooks);
 
-app.get("/", (req,res)=> res.send("Api is working!!"))
+app.get("/", (req,res)=> res.send("Api is working"))
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
-    console.log(`Server running on port : ${PORT}`)
+app.listen(PORT, (req,res)=>{
+    console.log(`Server is listening at port : ${PORT}`)
 })
